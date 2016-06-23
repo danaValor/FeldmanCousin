c2eb676120cbb6f187bf4cb409cfb1076bbbe8c3
danaValor <danaezd@sjtu.edu.cn>
2016-06-23 15:48:16 +0800

    writing a function to calcuate mu
    start

--- FeldmanCousin/main.cpp
+++ FeldmanCousin/main.cpp
@@ -1,9 +1,27 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include<TString.h>
+#include<iostream>
+using namespace std;
 
 int main()
 {
+
+		int const Nn = 100, bin = 5;
+		int nGenerated[bin] = { 95, 100, 130, 125, 110 };
+		int b[bin] = { 100 };
+		double mu[bin] = { 100, 110, 120, 130, 140 };
+		double chi = 0;
+		for (int i = 0; i < 5; i++) {
+			double chi0 = (nGenerated[i] - b[i] - mu[i])*(nGenerated[i] - b[i] - mu[i]) / (b[i] + mu[i]);
+			chi = chi + chi0;
+		}
+
+
+		cout << chi << endl;
+		getchar();
+		return 0;
+
 
 	TString a = "Hellow, ROOT";
 	printf("%s\n", (char const *)a);
---
+++ test1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int nG(double x, double y)
+{
+
+
+
+
+
+}
+
+
+int main()
+{
+
+
+
+
+
+	return 0;
+}
