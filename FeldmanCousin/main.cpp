53925bd9bbbdb3d2e6ef00452e959a5803c090fb
danaValor <danaezd@sjtu.edu.cn>
2016-06-30 18:38:32 +0800

    Chang1

--- FeldmanCousin/main.cpp
+++ FeldmanCousin/main.cpp
@@ -1,30 +1,313 @@
 
 #define _CRT_SECURE_NO_WARNINGS
+
+#define R__EXTERN       R__DllImport extern
+
 #include<TString.h>
+#include<TRandom3.h>
+#include<TMinuit.h>
+#include <TH2D.h>
+#include <TStyle.h>
+#include <TCanvas.h>
 #include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<algorithm>
+//#include<TFitterMinuit.h>
+//#include<TMinuit.h>
+//#include<TROOT.h>
+
 using namespace std;
 
-int main()
+TRandom3 rnd(0);
+int const bin = 5;
+int const background = 100;
+int const Nn = 1000;
+double Ebins[] = { 10, 20, 30, 40, 50, 60 };
+double L1 = 0.6, L2 = 1;
+static int nExperiment[bin];
+static int *nexp = &nExperiment[0];
+double initx = 0.0001;
+double xlower = -4.;
+int nx = 200, ny = 200;
+int const loopx = 5, loopy = 5;
+
+
+
+double Mu(double x, double y, int binNumber)
+{
+	double E2 = Ebins[binNumber + 1];
+	double E1 = Ebins[binNumber];
+
+
+	auto Simpson = [&](int n) {//a is lower bound, b is upper bound
+
+		double h = (E2 - E1) / n;
+		double e = E1, sum0 = 0, sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			double r = (L2 - L1 - 1 / (2 * 1.27 * y) * e * (sin(2 * 1.27 * y * L2 / e) -
+				sin(2 * 1.27 * y * L1 / e))) / 2;
+			if (i == 0 || i == n - 1) sum0 = r * h / 3;
+			else {
+				if (i % 2 == 1) sum0 = r * 2 * h / 3;
+				else sum0 = r * 4 * h / 3;
+			}
+			sum += sum0;
+			e += h;
+		};
+
+
+		return sum;
+	};
+	int const N = 3000;
+	double muNumber = x * Simpson(N) * 10000 / (L2 - L1) / (E2 - E1);// n = 1000, which is the number of integration bins
+
+	return muNumber;
+}
+
+
+
+int Ngenerated(double muIn)
 {
 
-		int const Nn = 100, bin = 5;
-		int nGenerated[bin] = { 95, 100, 130, 125, 110 };
-		int b[bin] = { 100 };
-		double mu[bin] = { 100, 110, 120, 130, 140 };
-		double chi = 0;
-		for (int i = 0; i < 5; i++) {
-			double chi0 = (nGenerated[i] - b[i] - mu[i])*(nGenerated[i] - b[i] - mu[i]) / (b[i] + mu[i]);
-			chi = chi + chi0;
-		}
+	double sigma = sqrt(muIn + background);
+	double mean = muIn + background;
+	double nn = rnd.Gaus(mean, sigma);
+	int ng = int(round(nn));
 
-
-		cout << chi << endl;
-		getchar();
-		return 0;
-
-
-	TString a = "Hellow, ROOT";
-	printf("%s\n", (char const *)a);
-	return 0;
+	return ng;
 
 }
+
+
+
+
+double Chi(double x, double y) //binNumber = 0.. 4
+{
+	double chiInit = 0;
+	for (int i = 0; i < bin; i++) {
+		chiInit += (nexp[i] - background - Mu(x, y, i)) *
+			(nexp[i] - background - Mu(x, y, i)) / (background + Mu(x, y, i));
+
+	}
+
+
+	return chiInit;
+
+}
+
+
+
+
+//int nPar = 2;
+
+void MinuitFunction(int& nDim, double* gout, double& result, double par[], int flg)
+{
+	//double a0 = 1.5;
+	result = Chi(par[0], par[1]);
+
+}
+
+
+double FindMinimum(double xI, double yI)
+{
+	//TMinuit minimizer(2);
+
+//	minimizer.mninit(5, 6, 7);
+//	minimizer.SetPrintLevel(-1);
+	int flag = 0;
+
+//	minimizer.SetFCN(MinuitFunction);
+//	minimizer.DefineParameter(0, "xx", xI, 0.01, 0, 1);
+//	minimizer.DefineParameter(1, "yy", yI, 1, 0, 1000);
+//	minimizer.mnexcm("SIMPLEX", 0, 0, flag);
+
+//	minimizer.SetErrorDef(1);
+
+//	minimizer.SetMaxIterations(500);
+//	int err = minimizer.Migrad();
+//	if (err != 0) {
+//		return err;
+//	}
+
+//	double xx1, mu1err;
+//	double yy1, mu2err;
+
+//	minimizer.GetParameter(0, xx1, mu1err);
+//	minimizer.GetParameter(1, yy1, mu2err);
+
+
+
+	//after finding the global minimum with simplex, use migrad to find the local minimum.
+	TMinuit minimizer1(2);
+	minimizer1.mninit(5, 6, 7);
+	minimizer1.SetPrintLevel(-1);
+	minimizer1.SetFCN(MinuitFunction);
+	minimizer1.DefineParameter(0, "xx2", xI, 0.01, 0, 1);
+	minimizer1.DefineParameter(1, "yy2", yI, 1, 0, 1000);
+	minimizer1.mnexcm("MIGRAD", 0, 0, flag);
+
+	minimizer1.SetErrorDef(1);
+
+	minimizer1.SetMaxIterations(500);
+	int err1 = minimizer1.Migrad();
+	if (err1 != 0) {
+		return err1;
+	}
+
+	double xx21, mu1err1;
+	double yy21, mu2err1;
+
+	minimizer1.GetParameter(0, xx21, mu1err1);
+	minimizer1.GetParameter(1, yy21, mu2err1);
+
+	double minimum = Chi(xx21, yy21);
+
+	return minimum;
+
+}
+
+
+
+double ChiCritical(double x, double y) {
+	double chiCritical[loopx][loopy] = { 0 };
+	double deltaChi[Nn] = { 0 };
+
+	double mu[bin] = { 0 };
+
+	//nExperiment[bin] = { 0 };
+
+	int cut = int(ceil(0.9 * Nn));
+
+
+	for (int p = 0; p < Nn; p++) {
+		for (int i = 0; i < bin; i++) {
+			mu[i] = Mu(x, y, i);
+
+			nExperiment[i] = Ngenerated(mu[i]);
+
+		}
+		double chiMin = FindMinimum(x, y);
+		//cout << chiMin << endl;
+		deltaChi[p] = Chi(x, y) - chiMin;
+		if (deltaChi[p] < 0) {
+			p = p - 1;
+			cout << "find minimum of chi-square again";
+		}
+
+	}
+
+	sort(deltaChi, deltaChi + Nn);
+	double chiCri = deltaChi[cut];
+	//cout << chiCri << endl;
+	return chiCri;
+
+}
+
+
+
+void WriteIn(double x,double y)
+{
+	char str[100];
+	sprintf(str, "data/x_%.4f_y_%.4f", x,y);
+	ofstream f1(str);
+	double chix1;
+	chix1 = ChiCritical(x,y);
+	f1 << chix1 << endl;
+	f1.close();
+}
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+	int i, j, n;
+
+//	printf("%s", argv[0]);
+	sscanf(argv[1], "%d", &i);
+	sscanf(argv[2], "%d", &j);
+	sscanf(argv[3], "%d", &n);
+//	printf("%d %d %d", i, j, n);
+
+
+	double x0 = initx, y0 = 1;
+
+	double logx0 = log10(x0);
+	double logy0 = log10(y0);
+	double hx = (0 - logx0) / (loopx - 1);
+	double hy = (3 - logy0) / (loopy - 1);
+	double chiCritical[loopx][loopy] = { 0 };
+	double deltaChi[Nn] = { 0 };
+
+
+	//double d = atof(argv[2]);
+	logx0 += i * hx;
+	logy0 += j * hy;
+	x0 = exp(log(10)*logx0);
+	y0 = exp(log(10)*logy0);
+	double chic1 = ChiCritical(x0, y0);
+
+
+	char str[100];
+	sprintf(str, "data/x_%d_y_%d", i, j);
+	ofstream f1(str);
+	f1 << chic1 << endl;
+	f1.close();
+
+
+
+//	if (0) {
+//		for (int n = 0; n < loopy; n++) {
+//			y0 = exp(log(10)*logy0);
+//			logx0 = log10(initx);
+//			for (int m = 0; m < loopx; m++) {
+//				x0 = exp(log(10)*logx0);
+
+//				chiCritical[m][n] = ChiCritical(x0, y0);
+//				logx0 += hx;
+//			}
+//			logy0 += hy;
+
+//		}
+//	}
+//	else {
+//
+//		double chiCritical1[5][5] = { {2.6154,2.5633,3.3945,3.8296,2.1542},
+//		{2.5511,2.7729,4.3495,4.5542,3.4050},{2.6892,3.7866,4.5823,4.5523,4.2425},
+//		{2.8240,4.1058,4.8172,4.529,4.8116},{3.571,4.1772,4.4678,4.5948,4.6152} };
+//		memcpy(chiCritical, chiCritical1, sizeof(chiCritical1));
+
+//	}
+
+
+
+
+	//final test
+	
+
+
+//	DrawDeltaChiC(chiCritical);
+//	DrawInter(chiCritical);
+//	DrawContour(chiCritical);
+
+
+
+	getchar();
+	
+
+
+	return 0;
+
+
+
+
+}
+
+
+
+
---
+++ PlotFC/plot.cpp
@@ -0,0 +1,265 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define R__EXTERN       R__DllImport extern
+
+#include<TString.h>
+#include<TRandom3.h>
+#include<TMinuit.h>
+#include <TH2D.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<algorithm>
+//#include<TFitterMinuit.h>
+//#include<TMinuit.h>
+//#include<TROOT.h>
+
+using namespace std;
+
+TRandom3 rnd(0);
+int const bin = 5;
+int const background = 100;
+int const Nn = 1000;
+double Ebins[] = { 10, 20, 30, 40, 50, 60 };
+double L1 = 0.6, L2 = 1;
+static int nExperiment[bin];
+static int *nexp = &nExperiment[0];
+double initx = 0.0001;
+double xlower = -4.;
+int nx = 200, ny = 200;
+int const loopx = 5, loopy = 5;
+
+
+
+double Mu(double x, double y, int binNumber)
+{
+	double E2 = Ebins[binNumber + 1];
+	double E1 = Ebins[binNumber];
+
+
+	auto Simpson = [&](int n) {//a is lower bound, b is upper bound
+
+		double h = (E2 - E1) / n;
+		double e = E1, sum0 = 0, sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			double r = (L2 - L1 - 1 / (2 * 1.27 * y) * e * (sin(2 * 1.27 * y * L2 / e) -
+				sin(2 * 1.27 * y * L1 / e))) / 2;
+			if (i == 0 || i == n - 1) sum0 = r * h / 3;
+			else {
+				if (i % 2 == 1) sum0 = r * 2 * h / 3;
+				else sum0 = r * 4 * h / 3;
+			}
+			sum += sum0;
+			e += h;
+		};
+
+
+		return sum;
+	};
+	int const N = 3000;
+	double muNumber = x * Simpson(N) * 10000 / (L2 - L1) / (E2 - E1);// n = 1000, which is the number of integration bins
+
+	return muNumber;
+}
+
+
+
+int Ngenerated(double muIn)
+{
+
+	double sigma = sqrt(muIn + background);
+	double mean = muIn + background;
+	double nn = rnd.Gaus(mean, sigma);
+	int ng = int(round(nn));
+
+	return ng;
+
+}
+
+
+
+
+double Chi(double x, double y) //binNumber = 0.. 4
+{
+	double chiInit = 0;
+	for (int i = 0; i < bin; i++) {
+		chiInit += (nexp[i] - background - Mu(x, y, i)) *
+			(nexp[i] - background - Mu(x, y, i)) / (background + Mu(x, y, i));
+
+	}
+
+}
+
+void DrawDeltaChiC(double(&ChiC)[loopx][loopy]) 
+{
+	TCanvas cvs;
+	cvs.SetWindowSize(200, 300);
+
+	int lbx = int(xlower);
+	TH2D hist("", "", loopx, lbx, 0, loopy, 0, 3);
+	for (int xi = 0; xi < loopx; ++xi) {
+		for (int yi = 0; yi < loopy; ++yi) {
+			//cout << xi << ";" << yi << endl;
+			hist.SetBinContent(xi + 1, yi + 1, ChiC[xi][yi]);
+		}
+	}
+	gStyle->SetOptStat(0);
+	hist.Draw("TEXT");
+	cvs.Print("ChiCTEXT2.png");
+	hist.Draw("COLZ");
+	cvs.Print("ChiCCOLZ2.png");
+}
+
+
+double Interpolate(double(&ChiC)[loopx][loopy], double x, double y) {
+	//cout << x << "x" << y << "y" << endl;
+	double xt = log10(x), yt = log10(y);
+	double xid = (xt - xlower) / (-xlower) * (loopx - 1);
+	double yid = yt / 3 * (loopy - 1);
+	int xlow = floor(xid);
+	int ylow = floor(yid);
+
+	//cout << xlow << ";" << ylow << endl;
+	if (xlow < 0) {
+		xlow = 0;
+		xid = xlow;
+	}
+	else  if (xlow >= loopx - 1) {
+		xlow = loopx - 2;
+		xid = xlow + 1;
+	}
+	if (ylow < 0) {
+		ylow = 0;
+		yid = ylow;
+	}
+	else  if (ylow >= loopy - 1) {
+		ylow = loopy - 2;
+		yid = ylow + 1;
+	}
+
+	int xup = xlow + 1;
+	int yup = ylow + 1;
+	double xlowWei = xup - xid;
+	double ylowWei = yup - yid;
+	//cout << xid << ";" << xup << ";" << xlow << endl;
+	double chiCr = xlowWei*ylowWei*ChiC[xlow][ylow]
+		+ xlowWei*(1 - ylowWei)*ChiC[xlow][yup]
+		+ (1 - xlowWei)*ylowWei*ChiC[xup][ylow]
+		+ (1 - xlowWei)*(1 - ylowWei)*ChiC[xup][yup];
+
+	return chiCr;
+
+}
+
+void DrawInter(double(&ChiC)[loopx][loopy]) {
+
+	TCanvas cvs;
+	gStyle->SetOptStat(0);
+	//cvs.SetWindowSize(200, 300);
+
+
+	TH2D hist("", "", nx, xlower, 0, ny, 0, 3);
+	//double xi1 = 0, yi1 = 0;
+	for (int xi = 0; xi < nx; ++xi) {
+		for (int yi = 0; yi < ny; ++yi) {
+			//xi1 = xi;
+			//yi1 = yi;
+			double xcare = exp(log(10)*(1.*xi / nx * (-xlower) + xlower));
+			double ycare = exp(log(10)*(1.*yi / ny * 3));
+			//double chi = Chi(xcare, ycare);
+			//cout << "xi" << xi << "yi" << yi << endl;
+
+			double chic = Interpolate(ChiC, xcare, ycare);
+
+			hist.SetBinContent(xi + 1, yi + 1, chic);
+		}
+	}
+	//double levels[] = { -1, 0,10 };
+	//hist.SetContour(3, levels);
+	//hist.Draw("TEXT");
+	//cvs.Print("ChiCInterpolateTEXT.png");
+	hist.Draw("COLZ");
+	cvs.Print("ChiCInterpolateCOLZ.png");
+
+}
+
+
+
+
+void DrawContour(double(&ChiC)[loopx][loopy]) {
+	TCanvas cvs;
+	gStyle->SetOptStat(0);
+	//cvs.SetWindowSize(200, 300);
+
+
+	TH2D hist("", "", nx, xlower, 0, ny, 0, 3);
+	//cout << "try_0" << endl;
+	for (int xi = 0; xi < nx; ++xi) {
+		for (int yi = 0; yi < ny; ++yi) {
+			//cout << xi << ";" << yi << endl;
+			double xcare = exp(log(10)*(1.*xi / nx*(-xlower) + xlower));
+			double ycare = exp(log(10)*(1.*yi / ny * 3));
+			double chi = Chi(xcare, ycare);
+			double chic = Interpolate(ChiC, xcare, ycare);
+			//cout << chic << endl;
+			double difference = chi - chic;
+			//cout << "chic = " << chic << endl;
+			//cout << "chi - chic = " << difference << endl;
+
+			if (difference > 15) { difference = 15; }
+			hist.SetBinContent(xi + 1, yi + 1, difference);
+		}
+	}
+	double levels[] = { -10,0 };
+	hist.SetContour(2, levels);
+	//cout << "try" << endl;
+	//hist.GetZaxis()->SetRangeUser(-4.61,0);
+	hist.Draw("SAME CONTZ");
+	//hist.Draw("colz");
+	cvs.Print("ChiContourCOLZ.png");
+
+
+
+
+}
+
+
+int main()
+{
+
+	ifstream f1;
+	char str[100];
+	double y = 10.;
+	sprintf(str, "Chidata/x_%.4fy_%.4f", y);
+
+	f1.open(str);
+	
+	double a;
+	f1 >> a;
+
+
+	double xTrue = 0.006;
+	double yTrue = 40;
+
+
+	double muT[bin];
+
+	for (int i = 0; i < bin; i++) {
+		muT[i] = Mu(xTrue, yTrue, i);
+
+		nExperiment[i] = Ngenerated(muT[i]);
+		cout << nExperiment[i] << endl;
+		//nExperiment[i] = 100;
+	}
+
+
+
+
+	R__EXTERN TStyle  *gStyle;
+	return 0;
+
+}
